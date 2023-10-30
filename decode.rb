3f339fb8a78915d0ef66798a1f422818879b4fb7
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-30 10:28:22 +0430

    add the gemfile

--- decode.rb
+++ decode.rb
@@ -40,7 +40,14 @@ def decode_char(morse_code)
     morse_code_ref[morse_code]
   end
   
-  
- 
-  
+  def decode_word(morse_word)
+    morse_word.split(' ').map { |morse_char| decode_char(morse_char) }.join('')
+  end
+  def decode(morse_message)
+    morse_words = morse_message.split('   ')
+    decoded = morse_words.map { |morse_word| decode_word(morse_word) }.join(' ')
+    return decoded
+  end
+  morse_message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+  puts decode(morse_message)
   
